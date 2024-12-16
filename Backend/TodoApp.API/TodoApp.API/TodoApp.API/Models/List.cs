0e4b19881f95ab8243252386abf11cfa561ba372
Pavol Toth <pavol.toth@kosickaakademia.sk>
2024-12-16 09:45:55 +0100

    Added list model

---
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/Models/List.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.API.Models;
+
+public class List
+{
+    public Guid Id { get; set; }
+    public int id_user { get; set; }
+    public string list_name { get; set; }
+    public int icon { get; set; }
+    public int color { get; set; }
+}

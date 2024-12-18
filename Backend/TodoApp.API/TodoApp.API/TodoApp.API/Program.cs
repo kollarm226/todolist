4e00194e50b17a40a851728a6bf4d5bc62d0b76b
Matej Kollár <matej.kollar@kosickaakademia.sk>
2024-12-18 08:37:56 +0100

    Added Swashbuckle.AspNetCore nuget package and created a connection with our project

--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/Program.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/Program.cs
@@ -34,22 +34,27 @@ using (MySqlConnection conn = new MySqlConnection(connectionString))
 }
 
 
-
-
-
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 // builder.Services.AddDbContext<TodoDbContext>(
 //     options => options.UseSqlServer(builder.Configuration.GetConnectionString("TodoDbConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c5a6971805fbc3c347b750deba6682fc370675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e514169e1f265656d18a2705b3a48f1f603a8afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

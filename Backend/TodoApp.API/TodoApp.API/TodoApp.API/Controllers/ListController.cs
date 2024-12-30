979266c7964002f515e32f352cc3db6c4f7c634b
Lukas Varga <lukas.varga@kosickaakademia.sk>
2024-12-30 16:26:16 +0100

    Added ListController.cs

---
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/Controllers/ListController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListsController : ControllerBase
+    {
+        private readonly TodoDbContext _dbContext;
+
+
+        public ListsController(TodoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetLists()
+        {
+
+            var lists = await _dbContext.Lists.ToListAsync();
+            return Ok(lists);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPost(int id)
+        {
+
+            var list = await _dbContext.Lists.FindAsync(id);
+
+
+            if (list == null)
+            {
+                return NotFound($"List with ID = {id} not found.");
+            }
+
+
+            return Ok(list);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllLists(int page = 1, int pageSize = 10)
+        {
+            var totalItems = await _dbContext.Lists.CountAsync();
+            var lists = await _dbContext.Lists
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+    
+            return Ok(new { TotalItems = totalItems, Lists = lists });
+        }
+
+    }
+}

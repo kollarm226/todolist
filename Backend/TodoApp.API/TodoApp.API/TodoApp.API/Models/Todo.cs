b053bcf538a4e9b59e939abe34e6c38471529b9c
Pavol Toth <pavol.toth@kosickaakademia.sk>
2025-01-10 14:37:58 +0100

    Changed name from done to isDone, added try and catch for tasks methods, added delete method and upgraded others methods for task.

--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/Models/Todo.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/Models/Todo.cs
@@ -8,16 +8,18 @@ namespace TodoApp.API.Models
         public int? idList { get; set; }
         public string name { get; set; }
         [DefaultValue(false)]
-        public bool done { get; set; }
+        public bool isDone { get; set; }
         public int? priority { get; set; }
         
         [DefaultValue(false)]
         public bool isDeleted { get; set; }
 
 
-        public Todo()
-        {
-            isDeleted = false;
-        }
+        // public Todo()
+        // {
+        //     isDeleted = false;               // This sets the default value of "isDeleted" to false.
+        //      isDone = false;                 // This sets the default value of "isDone" to false.
+        //
+        // }
     }
 }

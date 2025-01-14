5114f70451bc8e6818cf6905d0eee44ac8b0edd9
Pavol Toth <pavol.toth@kosickaakademia.sk>
2025-01-14 11:47:54 +0100

    added max lenght for list name

--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ef9c8205fcb7fd3652e86ef2dc332124ae7759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df2da0db39ed21871243e92148efc0748cbf769")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

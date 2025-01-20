0842bf329496b95378f209d38802ad719de5cbf5
Matej Kollár <matej.kollar@kosickaakademia.sk>
2025-01-20 11:27:15 +0100

    adjusted publish path

--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812b997e800ebf1afb3179e90048b74811499770")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd789f0ad9a3601f1eb356f562e7b0e636dc8c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

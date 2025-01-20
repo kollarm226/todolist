7b977efaec69a0bbd42964ea43722fac78a67ac6
Pavol Toth <pavol.toth@kosickaakademia.sk>
2025-01-20 13:22:44 +0100

    updated yaml

--- Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
+++ Backend/TodoApp.API/TodoApp.API/TodoApp.API/obj/Debug/net9.0/TodoApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359cf815e9a33949271d9db365d1ffa5eff1c4bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d083e1c55f54b48e15c8b46191473a41ad23bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

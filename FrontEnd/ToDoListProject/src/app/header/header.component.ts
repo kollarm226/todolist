355887f194d082d930a3cc70ae433de6a8721796
Tomas <tomas.oros0155@gmail.com>
2025-01-24 09:17:31 +0100

    fixed logo fading, list name was deleted

--- FrontEnd/ToDoListProject/src/app/header/header.component.ts
+++ FrontEnd/ToDoListProject/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router'; // Importuj Router
+import { Router, NavigationStart } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Panel } from 'primeng/panel';
 
@@ -15,31 +15,30 @@ import { Panel } from 'primeng/panel';
 })
 export class HeaderComponent implements OnInit {
   isFirstLoad = true;
-  isRegisterPage = false; // Nová premenná pre kontrolu stránky
+  isRegisterPage = false;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // Zistíme, či sme na stránke "register"
     this.isRegisterPage = this.router.url.includes('/register');
 
-    // Ak nie sme na stránke "register", animujeme logo
+
     if (!this.isRegisterPage && !sessionStorage.getItem('hasAnimated')) {
-      this.isFirstLoad = true; // Animácia sa spustí
+      this.isFirstLoad = true;
       setTimeout(() => {
         this.isFirstLoad = false;
-        sessionStorage.setItem('hasAnimated', 'true'); // Uložíme informáciu, že animácia prebehla
+        sessionStorage.setItem('hasAnimated', 'true');
       }, 3000);
     } else {
-      this.isFirstLoad = false; // Na stránke "register" animácia nebude
+      this.isFirstLoad = false;
     }
 
-    // Resetovanie animácie pri zmene cesty
+
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
-        this.isRegisterPage = this.router.url.includes('/register'); // Skontrolujeme, či sme na stránke "register"
+        this.isRegisterPage = this.router.url.includes('/register');
         if (!this.isRegisterPage) {
-          this.isFirstLoad = true; // Resetujeme animáciu, ak nie sme na "register" stránke
+          this.isFirstLoad = true;
           setTimeout(() => {
             this.isFirstLoad = false;
           }, 3000);
